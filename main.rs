2612c4a6d20a8614e09a1e6c17c83e8e8225786d
mathieu <mathieu.c.sabourin@gmail.com>
2016-10-01 22:06:57 -0700

    Better main.

--- main.rs
+++ main.rs
@@ -1,9 +1,7 @@
 extern crate bencode;
 
 fn main() {
-    println!("Hello, World!");
-
-    let string = "l1:a1:bd3:key5:value2:p1i5e2:p2i3eee";
+    let string = "l13:Hello, World!1:a1:bd3:key5:value2:p1i5e2:p2i3eee";
     let res = bencode::decode::parse(&string);
     println!("{:?}", res);
 }
