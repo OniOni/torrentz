7c8385e5fd8602a324b11a502079c8887f4e205a
mathieu <mathieu.c.sabourin@gmail.com>
2016-11-02 22:25:04 -0700

    Use bytes.

--- py/bencode.py
+++ py/bencode.py
@@ -1,15 +1,21 @@
-def parse_string(ite, length):
-    s = ""
+def parse_string(ite, c):
+    s = b""
+    while c != b':':
+        s += c
+        c = next(ite)
+
+    length = int(s)
+    s = b""
     for _ in range(length):
         s += next(ite)
 
     return s
 
 def parse_int(ite):
-    s = ""
+    s = b""
     while True:
         c = next(ite)
-        if c == 'e':
+        if c == b'e':
             return int(s)
         s += c
 
@@ -17,7 +23,7 @@ def parse_dict(ite):
     r = {}
     while True:
         c = next(ite)
-        if c == 'e':
+        if c == b'e':
             return r
 
         key = parse_element(ite, c)
@@ -28,7 +34,7 @@ def parse_list(ite):
     r = []
     while True:
         c = next(ite)
-        if c == 'e':
+        if c == b'e':
             return r
 
         r.append(parse_element(ite, c))
@@ -37,22 +43,34 @@ def parse_element(ite, p=None):
     r = None
     c = p or next(ite)
 
-    if c == 'd':
+    if c == b'd':
         r = parse_dict(ite)
-    elif c == 'l':
+    elif c == b'l':
         r = parse_list(ite)
-    elif c == 'i':
+    elif c == b'i':
         r = parse_int(ite)
-    elif c.isnumeric():
-        r = parse_string(ite, int(c))
+    elif c.isdigit():
+        r = parse_string(ite, c)
+    else:
+        print(c)
 
     return r
 
-def loads(s):
-    return parse_element(iter(s))
+def bytes_iter(s):
+    return map(lambda x: bytes([x]), s)
 
+def loads(s):
+    return parse_element(bytes_iter(s))
+
+def load(f):
+    return parse_element(bytes_iter(f.read()))
 
 if __name__ == '__main__':
-    s = "d5helloi42e2hili1ei2eee" # {'hello': 42, 'hi': [1, 2]}
+    s = b"d5:helloi42e2:hili1ei2eee" # {'hello': 42, 'hi': [1, 2]}
     r = loads(s)
-    print(s, '->', r)
+    print(r)
+
+    with open('ubuntu-16.04-server-amd64.iso.torrent', 'rb') as f:
+        r = load(f)
+
+    #print(r)
