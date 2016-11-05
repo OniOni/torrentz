f67ff9a9c00c8b2c79f63ba84dea85a60d35bc7d
mathieu <mathieu.c.sabourin@gmail.com>
2016-11-04 21:31:28 -0700

    Test setup.

---
+++ py/__main__.py
@@ -0,0 +1,17 @@
+import socket
+import ssl
+
+from . import client
+
+s = socket.socket()
+# secure_socket = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_SSLv2)
+s.connect(('localhost', 55496))
+
+with open('ubuntu-16.10-desktop-amd64.iso.torrent', 'rb') as f:
+    c = client.Client(f)
+
+msg = c.mk_handshake_msg()
+print(msg)
+s.send(msg)
+r = s.recv(4096)
+print(r)
