650c7c493721fe682c913908da0839ac66928faf
mathieu <mathieu.c.sabourin@gmail.com>
2016-11-07 18:50:54 -0800

    Cleanup

--- py/__main__.py
+++ py/__main__.py
@@ -4,9 +4,6 @@ import ssl
 from . import client
 
 s = socket.socket()
-# secure_socket = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_SSLv2)
-
-# pi ('192.168.1.12', 51413)
 s.connect(('localhost', 55496))
 
 with open('ubuntu-16.10-desktop-amd64.iso.torrent', 'rb') as f:
